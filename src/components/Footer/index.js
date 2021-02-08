399fca465181075419030d10a25ac9026c9daf61
JorbFreire <jorbfreire@ufrn.edu.br>
2021-02-08 01:22:46 -0300

    resets and login page

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import styles from './styles.module.css';
+
+export default function Home() {
+  return (
+    <footer className={styles.container}>
+      <p>Icons made by&nbsp;</p>
+      <a href="https://www.flaticon.com/authors/roundicons-freebies" title="Roundicons Freebies">
+        Roundicons Freebies&nbsp;
+      </a>
+      <p>from&nbsp;</p>
+      <a href="https://www.flaticon.com/" title="Flaticon">
+        www.flaticon.com
+      </a>
+    </footer>
+  );
+}
